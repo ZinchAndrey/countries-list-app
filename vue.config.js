34439c3f9a6e35dd329998f7a2ed5df8ba6b6295
ZinchAndrey <zinchandrey@gmail.com>
2022-08-31 20:24:10 +0300

    update router

--- vue.config.js
+++ vue.config.js
@@ -1,14 +1,21 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  // publicPath: '/countries-list-app/'
-});
+// const { defineConfig } = require('@vue/cli-service')
+// module.exports = defineConfig({
+//   transpileDependencies: true,
+//   publicPath: '/countries-list-app/'
+// });
 
 // vue.config.js
 
 /**
-//  * @type {import('@vue/cli-service').ProjectOptions}
-//  */
+ * @type {import('@vue/cli-service').ProjectOptions}
+ */
 //  module.exports = {
+  
 //   publicPath: '/countries-list-app/'
 // }
+
+module.exports = {
+  publicPath: process.env.NODE_ENV === 'production'
+  ? '/countries-list-app/' // note the trailing slash
+  : '/',
+}
